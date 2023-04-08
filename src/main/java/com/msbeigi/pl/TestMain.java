6e3962f47b3cf396478e868ceddc92b6d2526331
msbeigiai <msbeigi83@gmail.com>
2023-04-08 13:01:09 +0330

    Add command line functionality

--- src/main/java/com/msbeigi/pl/TestMain.java
+++ src/main/java/com/msbeigi/pl/TestMain.java
@@ -8,6 +8,7 @@ import com.msbeigi.person.User;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class TestMain {
     public static void main(String[] args) {
@@ -20,14 +21,35 @@ public class TestMain {
                 new Library(new Address("Nazi Abad", "Tehran"), "Rajaee", Arrays.asList(blackSwan, life));
         library.addUserToLibrary(mohsen);
 
-        System.out.println(library.allBooks());
+        /*System.out.println(library.allBooks());
         System.out.println(library.findBookById(library.allBooks().get(0).getId()));
         System.out.println(library.allUsers());
         System.out.println(library.findUserById(mohsen.getId()));
-        System.out.println(library.findUsersByName("Mohsen"));
+        System.out.println(library.findUsersByName("Mohsen"));*/
         User faezeh = new User("Zahra", 19, List.of(life, blackSwan));
         library.addUserToLibrary(faezeh);
-        System.out.println(library.allUsers());
+        // System.out.println(library.allUsers());
+
+        System.out.println("type 'l/u' to list all books/users in library: ");
+        System.out.println("type 'fbi' to find a book by its 'id': ");
+        System.out.println("type 'fun' to find a user by its 'name': ");
+
+        Scanner input = new Scanner(System.in);
+        while (input.hasNext()) {
+            switch (input.next()) {
+                case "l" -> System.out.println(library.allBooks());
+                case "u" -> System.out.println(library.allUsers());
+                case "fun" -> {
+                    Scanner scanner = new Scanner(System.in);
+                    System.out.println("provide user name: ");
+                    String userName = scanner.nextLine();
+                    System.out.println(library.findUsersByName(userName));
+                }
+                default ->
+                        System.out.println("please provide valid input 'l/u/fun' list all books/users and user by 'name'");
+            }
+        }
+
 
     }
 
